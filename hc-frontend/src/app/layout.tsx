ec23567c6c6f4dafd625404300f372d2b7d6d2d1
Suman Hansada <suman.hansada@gmail.com>
2025-03-24 15:27:17 +0530

    Added nunito font

--- hc-frontend/src/app/layout.tsx
+++ hc-frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Nunito } from "next/font/google";
 
 import { DeviceContextProvider } from "@/providers/DeviceContextProvider";
 import { DialogContextProvider } from "@/providers/DialogContextProvider";
@@ -21,7 +22,13 @@ import ReduxProvider from "@/providers/ReduxProvider";
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
+  weight: ["400", "500", "600", "700", "800"],
+});
+
+const nutino = Nunito({
+  variable: "--font-nunito",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
 });
 
 export const metadata: Metadata = {
@@ -38,8 +45,9 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content="#EA3934" />
       </head>
-      <body className={`${inter.variable}`}>
+      <body className={`${inter.variable} ${nutino.variable}`}>
         <ReduxProvider>
           <QueryProvider>
             <DeviceContextProvider>
