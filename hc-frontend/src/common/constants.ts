eaa8da958cacb37305027b4c66985782b867c6f1
Suman Hansada <suman.hansada@gmail.com>
2025-10-17 00:57:31 +0530

    Corrected API endpoint

--- hc-frontend/src/common/constants.ts
+++ hc-frontend/src/common/constants.ts
@@ -1,6 +1,6 @@
 export const BASE_API_URL =
   process.env.NEXT_PUBLIC_HOUSECLAY_API_BASE_URL ||
-  "http://ec2-13-201-0-200.ap-south-1.compute.amazonaws.com:8080/api";
+  "https://apis.houseclay.com/api";
 export const SUPPORT_EMAIL = "support@houseclay.com";
 export const SUPPORT_CONTACT = "+91 7892014327";
 
