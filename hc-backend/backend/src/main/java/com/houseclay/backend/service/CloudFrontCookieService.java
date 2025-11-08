94c25c83720b11ab294cfebbd2e2e0ed9ef3002a
Rohit Dayama <dayamarohit710@gmail.com>
2025-11-08 23:58:10 +0530

    Changed logic for generating cookie from env to file

--- hc-backend/backend/src/main/java/com/houseclay/backend/service/CloudFrontCookieService.java
+++ hc-backend/backend/src/main/java/com/houseclay/backend/service/CloudFrontCookieService.java
@@ -4,34 +4,29 @@ import com.amazonaws.services.cloudfront.CloudFrontCookieSigner;
 import com.amazonaws.services.cloudfront.util.SignerUtils;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Service;
-import org.springframework.beans.factory.annotation.Value;
-import java.util.Base64;
-import java.security.PrivateKey;
+
+import java.io.File;
 import java.time.Instant;
 import java.util.Date;
 import java.util.Map;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.security.KeyFactory;
 
 @Service
 public class CloudFrontCookieService {
 
-    @Value("${cloudfront.private-key}")
-    private String privateKeyPem;
-
     private static final String KEY_PAIR_ID = "K17ZTW5NPXMRFP";
+    private static final String PRIVATE_KEY_PATH = "/home/ec2-user/backend/private_key.pem";
     private static final String DISTRIBUTION_DOMAIN = "cdn.houseclay.com";
 
     public Map<String, ResponseCookie> generateSignedCookies() throws Exception {
         String resourcePath = "https://" + DISTRIBUTION_DOMAIN + "/properties/*" ;
         Date expires = Date.from(Instant.now().plusSeconds(3600)); // 1 hour validity
-        PrivateKey privateKey = loadPrivateKeyFromPem(privateKeyPem);
+        File privateKeyFile = new File(PRIVATE_KEY_PATH);
 
         CloudFrontCookieSigner.CookiesForCustomPolicy cookies =
                 CloudFrontCookieSigner.getCookiesForCustomPolicy(
                         SignerUtils.Protocol.https,
                         DISTRIBUTION_DOMAIN,
-                        privateKey,
+                        privateKeyFile,
                         resourcePath,
                         KEY_PAIR_ID,
                         expires,
@@ -69,16 +64,5 @@ public class CloudFrontCookieService {
                 cookies.getSignature().getKey(), signatureCookie,
                 cookies.getKeyPairId().getKey(), keyPairCookie
         );
-    }
-
-    private PrivateKey loadPrivateKeyFromPem(String pem) throws Exception {
-        String sanitizedPem = pem
-                .replace("-----BEGIN PRIVATE KEY-----", "")
-                .replace("-----END PRIVATE KEY-----", "")
-                .replaceAll("\\s+", "");
-
-        byte[] keyBytes = Base64.getDecoder().decode(sanitizedPem);
-        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
-        return KeyFactory.getInstance("RSA").generatePrivate(keySpec);
     }
 }
