f1f46bbe2f7dfd464f8d667bfd83101ebaa08137
Suman Hansada <suman.hansada@gmail.com>
2025-11-27 13:42:47 +0530

    Added back unoptimized

--- hc-frontend/src/utility-components/ImageWithLoader.tsx
+++ hc-frontend/src/utility-components/ImageWithLoader.tsx
@@ -61,6 +61,7 @@ const ImageWithLoader = memo(function ImageWithLoader({
           className={`object-cover ${className ?? ""} ${isLoading ? "opacity-0" : "opacity-100"} transition-opacity duration-300`}
           onLoad={handleLoad}
           onError={handleError}
+          unoptimized
           {...rest}
         />
       )}
