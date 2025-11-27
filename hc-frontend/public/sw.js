74ca70ac0c7fc23fd6aa08f04574fca0f3fc79f6
Suman Hansada <suman.hansada@gmail.com>
2025-11-27 12:57:30 +0530

    Removed unwanted cache

--- hc-frontend/public/sw.js
+++ hc-frontend/public/sw.js
@@ -2,8 +2,6 @@
 const CACHE_NAME = "houseclay-v1";
 const urlsToCache = [
   "/",
-  "/static/js/bundle.js",
-  "/static/css/main.css",
   "/manifest.json",
   "/web-app-manifest-192x192.png",
   "/web-app-manifest-512x512.png",
