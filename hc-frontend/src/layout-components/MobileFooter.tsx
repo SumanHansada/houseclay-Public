91430b77a1678c982abdcd882112efecf9c7d3e5
Suman Hansada <suman.hansada@gmail.com>
2025-12-24 19:22:17 +0530

    Minor fix in mobile footer for android

--- hc-frontend/src/layout-components/MobileFooter.tsx
+++ hc-frontend/src/layout-components/MobileFooter.tsx
@@ -13,9 +13,9 @@ const MobileFooter: React.FC<MobileFooterProps> = ({
 }) => {
   return (
     <footer
-      className={`fixed bottom-0 left-0 right-0 mx-auto px-4 py-3 border-t border-t-gray-300 bg-white z-50 md:hidden`}
+      className={`fixed bottom-0 left-0 right-0 pb-safe-bottom border-t border-t-gray-200 bg-white z-50 md:hidden`}
     >
-      <div className={`flex justify-between w-full ${className}`}>
+      <div className={`flex justify-between w-full px-4 py-2 ${className}`}>
         {children}
       </div>
     </footer>
