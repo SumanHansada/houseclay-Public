03650ee03e5b65ef0f19e10ea0bcebab14c46b78
Aryan Riyal <aryan13.riyal@gmail.com>
2026-01-30 18:12:05 +0530

    removed previous id's from Bundle enum because of external_payments_bundle_check constraint

--- hc-backend/backend/src/main/java/com/houseclay/backend/repository/UserRepository.java
+++ hc-backend/backend/src/main/java/com/houseclay/backend/repository/UserRepository.java
@@ -6,6 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-    User findByCorporateEmailID(String corporateEmailID);
     boolean existsByCorporateEmailID(String corporateEmailID);
 }
