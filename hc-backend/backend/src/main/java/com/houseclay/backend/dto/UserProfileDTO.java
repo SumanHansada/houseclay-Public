431b442163807d8af8e9db66e55775943d2d9ca8
Aryan Riyal <aryan13.riyal@gmail.com>
2026-02-27 12:54:06 +0530

    added new fields of User Entity to UserDetails related DTOs

--- hc-backend/backend/src/main/java/com/houseclay/backend/dto/UserProfileDTO.java
+++ hc-backend/backend/src/main/java/com/houseclay/backend/dto/UserProfileDTO.java
@@ -10,8 +10,11 @@ public class UserProfileDTO {
     private String email;
     private String name;
     private boolean isEmailVerified;
+    private boolean isCorporateEmailVerified;
     private boolean onWhatsApp;
     private int connectBal;
+    private String companyName;
+    private String jobTitle;
     private List<OwnedPropertyDTO> ownedProperties;
     private List<PropertyCardDTO> shortlistedProperties;
     private List<PropertyCardDTO> contactedProperties;
